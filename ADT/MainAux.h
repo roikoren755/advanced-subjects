fdf2fb3784990b2a225bd39fe081547ab7f03fc9
nadav352 <33449831+nadav352@users.noreply.github.com>
2018-04-15 10:05:38 +0300

    print to file

--- ADT/MainAux.h
+++ ADT/MainAux.h
@@ -6,12 +6,16 @@
 #define ADVANCED_SUBJECTS_MAINAUX_H
 
 #include "RPSGame.h"
+#define ALL_FLAGS_CAPTURED -2
+#define ALL_MOVING_PIECES_CAPTURED -3
+#define LEGAL_TIE -4
 
 class MainAux {
 public:
 	static int rpsPrintGamePositionErrorResult(RPSGame &rpsGame,int player1lineError,int player2lineError);
+	static int rpsPrintGameResult(RPSGame &game,int reason);
 	static int rpsLoadPositionFile(RPSGame &rpsGame, std::string &positionFile, int player);
-	static int rpsPlayTwoPlayerMoves(RPSGame &rpsGame, std::string &player1MoveFile, std::string &player2MoveFile);
+	static RPS_Message rpsPlayTwoPlayerMoves(RPSGame &rpsGame, std::string &player1MoveFile, std::string &player2MoveFile);
 };
 
 #endif //ADVANCED_SUBJECTS_MAINAUX_H
