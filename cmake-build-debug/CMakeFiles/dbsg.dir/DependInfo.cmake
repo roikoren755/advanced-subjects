44b5d2bcdedfab4bab1fa26ae80b0a3b98477acd
nadav352 <33449831+nadav352@users.noreply.github.com>
2018-05-15 21:35:28 +0300

    cmake

---
+++ cmake-build-debug/CMakeFiles/dbsg.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/user/CLionProjects/advanced-subjects/ex2/RPSBoard.cpp" "C:/Users/user/CLionProjects/advanced-subjects/cmake-build-debug/CMakeFiles/dbsg.dir/ex2/RPSBoard.cpp.obj"
+  "C:/Users/user/CLionProjects/advanced-subjects/ex2/RPSFilePlayerAlgorithm.cpp" "C:/Users/user/CLionProjects/advanced-subjects/cmake-build-debug/CMakeFiles/dbsg.dir/ex2/RPSFilePlayerAlgorithm.cpp.obj"
+  "C:/Users/user/CLionProjects/advanced-subjects/ex2/RPSGame.cpp" "C:/Users/user/CLionProjects/advanced-subjects/cmake-build-debug/CMakeFiles/dbsg.dir/ex2/RPSGame.cpp.obj"
+  "C:/Users/user/CLionProjects/advanced-subjects/ex2/RPSPiece.cpp" "C:/Users/user/CLionProjects/advanced-subjects/cmake-build-debug/CMakeFiles/dbsg.dir/ex2/RPSPiece.cpp.obj"
+  "C:/Users/user/CLionProjects/advanced-subjects/ex2/main.cpp" "C:/Users/user/CLionProjects/advanced-subjects/cmake-build-debug/CMakeFiles/dbsg.dir/ex2/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/dbsg.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dbsg.dir/ex2/main.cpp.obj"
+  "CMakeFiles/dbsg.dir/ex2/RPSGame.cpp.obj"
+  "CMakeFiles/dbsg.dir/ex2/RPSPiece.cpp.obj"
+  "CMakeFiles/dbsg.dir/ex2/RPSBoard.cpp.obj"
+  "CMakeFiles/dbsg.dir/ex2/RPSFilePlayerAlgorithm.cpp.obj"
+  "dbsg.pdb"
+  "dbsg.exe"
+  "dbsg.exe.manifest"
+  "libdbsg.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dbsg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
